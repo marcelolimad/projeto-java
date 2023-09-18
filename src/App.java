3127492d824b3a950904c5c29bb2c0ee1ca61636
marcelolimad <marcelolimad@gmail.com>
2023-09-18 20:30:04 -0300

    teste

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Mundo!  teste ok");
+        System.out.println("Hello, Mundo!  teste ok ok ok");
     }
 }
